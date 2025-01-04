af9777e28d07ab6462317bbea801f2e3fb5b7804
nhan nguyen <nhanhn1@uci.edu>
2025-01-03 22:28:36 -0800

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/vite-react-deploy/",
   plugins: [react()],
   css: {
     modules: {
