75735b866db9b43037e586cb40d850ff10a9674c
nhan nguyen <nhanhn1@uci.edu>
2025-01-03 22:39:15 -0800

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vite-react-deploy/",
+  base: "/personal-website/",
   plugins: [react()],
   css: {
     modules: {
